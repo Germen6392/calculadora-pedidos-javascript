55e31a68baa4ddaf8ef2a9f72d5d0b2a870042d0
Germen6392 <gerbarinas25@gmail.com>
2023-03-02 20:38:18 -0300

    variable adicional agregada

--- calculadora.js
+++ calculadora.js
@@ -2,21 +2,18 @@
 const ordenLocal = 90;
 const ordenCliente = 45;
 const km = 30;
-const adicional = 12;
 
 // Variables
 const distanciaInicial = document.getElementById("distancia-inicio");
 const distanciaFinal = document.getElementById("distancia-fin");
+const adicional = document.getElementById("adicional");
 const ganancia = document.getElementById("resultado");
 const suma = document.getElementById("sumar");
 const btncalcular = document.getElementById("enviar-operacion");
 
-btncalcular.addEventListener("click",(e)=> {
-console.log(e);
-});
-
+//Función que calcula las ganancias
 suma.addEventListener("click",(e)=>{
 e.preventDefault();
-ganancia.value =(ordenLocal + ordenCliente + distanciaInicial.value*km + distanciaFinal.value*km + adicional*distanciaFinal.value);
+ganancia.value =(ordenLocal + ordenCliente + distanciaInicial.value*km + distanciaFinal.value*km + adicional.value*distanciaFinal.value);
 })
 
